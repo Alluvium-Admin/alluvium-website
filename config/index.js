e0ab6706e82b5ed227126b4160e32b05dda4fd0d
Onwuka Stanley <kachi2505@yahoo.com>
2021-04-22 18:00:18 +0100

    Production link fix

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://website.com';
+export const server = dev ? 'http://localhost:3000' : 'https://focused-leakey-25e268.netlify.app/';
