f26429759fe12b122e89fa12f05ab06617c5abc0
Onwuka Stanley <kachi2505@yahoo.com>
2021-05-06 21:26:38 +0100

    Server link update

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const dev = process.env.NODE_ENV !== 'production';
 
 // export const server = dev ? 'http://localhost:3000' : 'https://../pages';
 
-export const server = dev ? 'http://localhost:3000' : "https://alluvium-website.vercel.app";
+export const server = dev ? 'http://localhost:3000' : "https://alluvium.net/";
 
 export const baseURL = 'http://localhost:3000';
 
