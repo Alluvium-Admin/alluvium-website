437243157a4bed442f420f440ca19b361d1c15d6
stanislaus-onwuka <kachi2505@yahoo.com>
2021-07-08 15:53:29 +0000

    Google analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,30 @@
 import { useEffect } from 'react';
+import { useRouter } from 'next/router'
+import * as ga from '../lib/ga'
 import '../styles/globals.css'
 import Layout from '../components/layout';
 import AOS from 'aos';
-import ReactGA from 'react-ga';
 import 'aos/dist/aos.css';
 import { ToastProvider } from 'react-toast-notifications';
 
 
 function MyApp({ Component, pageProps }) {
 
-  // Google Analytics
-  const MEASUREMENT_ID = "G-V7YCNDR60Z";
-
-  useEffect(() => {
-    ReactGA.initialize(MEASUREMENT_ID);
-  })
-  
-
   useEffect(()=>{
     AOS.init();
-  },[])
+  }, [])
+  
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return( 
     <ToastProvider placement="top-center">
